251aca721347d67b2b8e269831cc15bcb75ec366
Gaus Saraf Murady <63799660+b1tranger@users.noreply.github.com>
2025-02-19 21:46:00 +0600

    added external script

---
+++ myscripts.js
@@ -0,0 +1,23 @@
+let currentZoom = 1;
+    const iframe = document.querySelector('.iframe-4');
+
+    function zoomIn() {
+        currentZoom += 0.1;
+        updateZoom();
+    }
+
+    function zoomOut() {
+        if (currentZoom > 0.5) {
+            currentZoom -= 0.1;
+            updateZoom();
+        }
+    }
+
+    function resetZoom() {
+        currentZoom = 1;
+        updateZoom();
+    }
+
+    function updateZoom() {
+        iframe.style.transform = `scale(${currentZoom})`;
+    }
