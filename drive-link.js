34ffe8c03601300478447c73ae4606d953491a85
Gaus Saraf Murady <63799660+b1tranger@users.noreply.github.com>
2025-03-02 05:40:05 +0600

    added G-Drive archive
    rearranged some links

--- drive-link.js
+++ drive-link.js
@@ -1,13 +1,15 @@
 // Create a file called shared-elements.js
 document.addEventListener('DOMContentLoaded', function() {
     const navHTML = `
-      <a class="active-1" href="https://github.com/b1tranger/oUITS-Resources/releases/download/test/_oUITSr_appsgeyser.apk" 
-      target="_blank"><small>web-app</small></a>
+      <a href="https://drive.google.com/drive/folders/1iLo7FAsyDo2g9N3hvmO52ndDtsvTunWB"
+                        target="_blank">
+                        G-Drive Archive
+                    </a>
       <!-- Add other navigation links here -->
     `;
     
     // Insert the nav HTML into any element with class "navigation"
-    document.querySelectorAll('.navigation1').forEach(nav => {
+    document.querySelectorAll('.navigation2').forEach(nav => {
       nav.innerHTML = navHTML;
     });
   });
