0066d415a16ed03bb5e85f451f18792e5e8498c2
Gaus Saraf Murady <63799660+b1tranger@users.noreply.github.com>
2025-03-18 21:27:10 +0600

    updated backup

--- drive-link.js
+++ drive-link.js
@@ -1,7 +1,7 @@
 // Create a file called shared-elements.js
 document.addEventListener('DOMContentLoaded', function() {
     const navHTML = `
-      <a href="https://drive.google.com/drive/folders/1iLo7FAsyDo2g9N3hvmO52ndDtsvTunWB"
+      <a href="https://drive.google.com/drive/folders/1xzefPIJwRymaN-cmVPPZUx_YHnUtOaM_?usp=sharing"
                         target="_blank">
                         G-Drive Archive
                     </a>
