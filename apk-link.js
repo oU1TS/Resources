d4e5a8a3d870d6714ba2fbe8ca5a24b52caa57d7
Gaus Saraf Murady <63799660+b1tranger@users.noreply.github.com>
2025-03-30 17:33:39 +0600

    no clue what I did here

--- apk-link.js
+++ apk-link.js
@@ -1,6 +1,7 @@
 // Create a file called shared-elements.js
 document.addEventListener('DOMContentLoaded', function() {
     const navHTML = `
+    
       <a class="active-1" href="https://github.com/b1tranger/oUITS-Resources/releases/download/test/_oUITSr_appsgeyser.apk" 
       target="_blank"><small>web-app</small></a>
       <!-- Add other navigation links here -->
