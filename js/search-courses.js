46e07de21695b1e98697cbb41356a00757e148b0
Gaus Saraf Murady <63799660+b1tranger@users.noreply.github.com>
2025-04-12 03:16:04 +0600

    Update search-courses.js

--- js/search-courses.js
+++ js/search-courses.js
@@ -1,12 +1,15 @@
 // Sample course data with drive links
 const courses = [
+    // ----------------------------------------------------------------------
+    // ---------------------------####S01####--------------------------------
+    // ----------------------------------------------------------------------
     { code: "CSE0613111 + CSE0613112", title: "Structured Programming Language (+Lab) - SPL", department: "CSE", driveLink: "https://drive.google.com/drive/folders/1nSuCFox1x5rz02HhS0Gfu35b7W74k27x?usp=drive_link" },
     { code: "MATH0541111", title: "Differential and Integral Calculus", department: "CSE", driveLink: "https://drive.google.com/drive/folders/18Tv2KPjLAdIPOBv998lwvs28Y17dXRLE?usp=drive_link" },
     { code: "PHY0533111 + PHY0533112", title: "Physics (+lab)", department: "CSE", driveLink: "https://drive.google.com/drive/folders/1i0fmFBuSl-yzHV6e7aBU124eWqQpXnHg?usp=drive_link" },
     { code: "CHEM0531111", title: "Chemistry", department: "CSE", driveLink: "https://drive.google.com/drive/folders/1sk_4HSbvdA_cxSqwjOdOQ8ud4ctY6qt2?usp=drive_link" },
     { code: "GED0232111 + GED0232112", title: "English (+Lab)", department: "CSE", driveLink: "https://drive.google.com/drive/folders/19LPsLUN5QCBplYGDyy1b3f5Z2IZVIG3S?usp=drive_link" },
     // ----------------------------------------------------------------------
-    // ----------------------------------------------------------------------
+    // ---------------------------####S02####--------------------------------
     // ----------------------------------------------------------------------
     { code: "Bangladesh Studies: History and Culture", title: "GED0222121", department: "CSE", driveLink: "https://drive.google.com/drive/folders/1cbmYREhfX7_1VePKk-RPW26NCqbBrWYe?usp=drive_link" },
     { code: "CSE0541123", title: "Discrete Mathematics", department: "CSE", driveLink: "https://drive.google.com/drive/folders/15DOOPBwkSfuimrrREW45jVcevx07-8nu?usp=drive_link" },
@@ -16,7 +19,7 @@ const courses = [
     { code: "MATH0541121", title: "Ordinary and Partial Differential Equations", department: "CSE", driveLink: "https://drive.google.com/drive/folders/1_Zqg9MyV8XE0950vrqODdEVSlpYwnsZ-?usp=drive_link" },
     { code: "CSE0613124", title: "Web Application Design", department: "CSE", driveLink: "https://drive.google.com/drive/folders/1tjlH5xP-PlcosHZxcPwvinQE-wAUzOZS?usp=drive_link" },
     // ----------------------------------------------------------------------
-    // ----------------------------------------------------------------------
+    // ---------------------------####S03####--------------------------------
     // ----------------------------------------------------------------------
     { code: "MATH0541211", title: "Coordinate Geometry, Linear Algebra and Vector Analysis", department: "CSE", driveLink: "https://drive.google.com/drive/folders/1bpqvXJuu2qPZ-hVtkK4D9kotKvlqMtH0?usp=drive_link" },
     { code: "CSE0612223", title: "Data Communication", department: "CSE", driveLink: "https://drive.google.com/drive/folders/1G5HhXeaaESsUUzvshJI_-UFpQOSMZL-s?usp=drive_link" },
@@ -25,7 +28,7 @@ const courses = [
     { code: "CSE0613211 + CSE0613212", title: "Data Structures and Algorithms 1 ", department: "CSE", driveLink: "https://drive.google.com/drive/folders/1pmKWdksSPezGZdPct2gAydYctKFJMvpI?usp=drive_link" },
     // { code: "", title: "", department: "CSE", driveLink: "" },
     // ----------------------------------------------------------------------
-    // ----------------------------------------------------------------------
+    // ---------------------------####S04####--------------------------------
     // ----------------------------------------------------------------------
     { code: "GED113", title: "Financial and Managerial Accounting", department: "CSE", driveLink: "https://drive.google.com/drive/folders/1achIW_ZkLk7SUTcCBIamg7CHvvl2yeSb?usp=drive_link" },
     { code: "MAT265", title: "Complex Variables, Fourier Analysis and Laplace Transform", department: "CSE", driveLink: "https://drive.google.com/drive/folders/1WVfboSGhSvmWjYBU23s2Bk79JX7SyoI2?usp=drive_link" },
@@ -34,16 +37,7 @@ const courses = [
     { code: "CSE217", title: "Linear Algebra, Probability and Statistics for Engineers", department: "CSE", driveLink: "https://drive.google.com/drive/folders/1wFrG_OxI7-BzFHadiAX9aFJRPVH0_TgW?usp=drive_link" },
     { code: "CSE213", title: "Theory of Computation", department: "CSE", driveLink: "https://drive.google.com/drive/folders/1Lk_dkegB5YNBlQpYrOIcUP0KYS7e-oo0?usp=drive_link" },
     // ----------------------------------------------------------------------
-    // ----------------------------------------------------------------------
-    // ----------------------------------------------------------------------
-    { code: "", title: "", department: "CSE", driveLink: "" },
-    { code: "", title: "", department: "CSE", driveLink: "" },
-    { code: "", title: "", department: "CSE", driveLink: "" },
-    { code: "", title: "", department: "CSE", driveLink: "" },
-    { code: "", title: "", department: "CSE", driveLink: "" },
-    { code: "", title: "", department: "CSE", driveLink: "" },
-    // ----------------------------------------------------------------------
-    // ----------------------------------------------------------------------
+    // ---------------------------####S04####--------------------------------
     // ----------------------------------------------------------------------
     { code: "", title: "", department: "CSE", driveLink: "" },
     { code: "", title: "", department: "CSE", driveLink: "" },
@@ -52,16 +46,7 @@ const courses = [
     { code: "", title: "", department: "CSE", driveLink: "" },
     { code: "", title: "", department: "CSE", driveLink: "" },
     // ----------------------------------------------------------------------
-    // ----------------------------------------------------------------------
-    // ----------------------------------------------------------------------
-    { code: "", title: "", department: "CSE", driveLink: "" },
-    { code: "", title: "", department: "CSE", driveLink: "" },
-    { code: "", title: "", department: "CSE", driveLink: "" },
-    { code: "", title: "", department: "CSE", driveLink: "" },
-    { code: "", title: "", department: "CSE", driveLink: "" },
-    { code: "", title: "", department: "CSE", driveLink: "" },
-    // ----------------------------------------------------------------------
-    // ----------------------------------------------------------------------
+    // ---------------------------####S05####--------------------------------
     // ----------------------------------------------------------------------
     { code: "", title: "", department: "CSE", driveLink: "" },
     { code: "", title: "", department: "CSE", driveLink: "" },
@@ -70,7 +55,25 @@ const courses = [
     { code: "", title: "", department: "CSE", driveLink: "" },
     { code: "", title: "", department: "CSE", driveLink: "" },
     // ----------------------------------------------------------------------
+    // ---------------------------####S06####--------------------------------
     // ----------------------------------------------------------------------
+    { code: "", title: "", department: "CSE", driveLink: "" },
+    { code: "", title: "", department: "CSE", driveLink: "" },
+    { code: "", title: "", department: "CSE", driveLink: "" },
+    { code: "", title: "", department: "CSE", driveLink: "" },
+    { code: "", title: "", department: "CSE", driveLink: "" },
+    { code: "", title: "", department: "CSE", driveLink: "" },
+    // ----------------------------------------------------------------------
+    // ---------------------------####S07####--------------------------------
+    // ----------------------------------------------------------------------
+    { code: "", title: "", department: "CSE", driveLink: "" },
+    { code: "", title: "", department: "CSE", driveLink: "" },
+    { code: "", title: "", department: "CSE", driveLink: "" },
+    { code: "", title: "", department: "CSE", driveLink: "" },
+    { code: "", title: "", department: "CSE", driveLink: "" },
+    { code: "", title: "", department: "CSE", driveLink: "" },
+    // ----------------------------------------------------------------------
+    // ---------------------------####S08####--------------------------------
     // ----------------------------------------------------------------------
 ];
 
