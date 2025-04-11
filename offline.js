8af23349a24f68cb7ea5e61b1851c00cb455cb17
Gaus Saraf Murady <63799660+b1tranger@users.noreply.github.com>
2025-04-11 21:40:04 +0600

    Update offline.js

--- offline.js
+++ offline.js
@@ -8,22 +8,30 @@ window.onload = function() {
 };
 
 // Register Service Worker
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-        .then((reg) => {
-            console.log("Service Worker Registered");
 
-            // Check for updates
-            reg.addEventListener('updatefound', () => {
-                const newWorker = reg.installing;
-                newWorker.addEventListener('statechange', () => {
-                    if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
-                        if (confirm("New version available. Refresh now?")) {
-                            window.location.reload();
-                        }
-                    }
-                });
-            });
-        })
-        .catch(err => console.log("Service Worker Registration Failed:", err));
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/sw.js")
+        .then(() => console.log("Service Worker Registered"))
+        .catch((error) => console.log("Service Worker Registration Failed:", error));
 }
+
+
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('/sw.js')
+//         .then((reg) => {
+//             console.log("Service Worker Registered");
+
+//             // Check for updates
+//             reg.addEventListener('updatefound', () => {
+//                 const newWorker = reg.installing;
+//                 newWorker.addEventListener('statechange', () => {
+//                     if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+//                         if (confirm("New version available. Refresh now?")) {
+//                             window.location.reload();
+//                         }
+//                     }
+//                 });
+//             });
+//         })
+//         .catch(err => console.log("Service Worker Registration Failed:", err));
+// }
