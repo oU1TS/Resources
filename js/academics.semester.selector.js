f892ab05aafbbe31f9e94c87350088aca4924f14
Gaus Saraf Murady <63799660+b1tranger@users.noreply.github.com>
2025-06-21 05:33:59 +0600

    commit-20

--- js/academics.semester.selector.js
+++ js/academics.semester.selector.js
@@ -79,7 +79,7 @@ const departmentDropdown = document.getElementById('departmentDropdown');
         // Reset functionality
         resetButton.addEventListener('click', () => {
           // Reset dropdown header
-          dropdownHeader.textContent = 'Select you Department...';
+          dropdownHeader.textContent = 'Select Department';
     
           // Clear semester list
           semesterList.innerHTML = "";
